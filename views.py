2a571e7f87c170dc73370320a650f6b48189a9ba
jonas.erik.vilhelm.werner@gmail.com <jonas.erik.vilhelm.werner@gmail.com>
2017-12-22 10:30:55 +0900

    taskr part 1 complete

--- views.py
+++ views.py
@@ -110,6 +110,18 @@ def complete(task_id):
     flash('The task was marked as complete.')
     return redirect(url_for('tasks'))
 
+@app.route('/notcomplete/<int:task_id>/')
+@login_required
+def notcomplete(task_id):
+    g.db = connect_db()
+    g.db.execute(
+        'UPDATE tasks SET status=1 WHERE task_id='+str(task_id)
+    )
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as not yet complete.')
+    return redirect(url_for('tasks'))
+
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete_entry(task_id):
